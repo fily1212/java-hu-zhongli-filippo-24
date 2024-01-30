77843a32102d8bcff5076ba7b07a62a14c4651df
fily1212 <fily1212@gmail.com>
2024-01-30 15:54:51 +0100

    operazioni e scanner

--- Lezione1/HelloWorld.java
+++ Lezione1/HelloWorld.java
@@ -1,5 +1,12 @@
 public class HelloWorld {
     public static void main(String args[]) {
         System.out.println("Hello world");
+        int a = 4;
+        int b = a + 2; 
+        System.out.println(a);
+        double c = 4.2;
+        String nome = "Ciao";
+
     }
+    
 }
