2ee44ee44bf9040e00abac3379506fa28d247a03
fily1212 <fily1212@gmail.com>
2024-02-06 14:01:10 +0100

    lezione 2 es 2

--- Lezioni/src/lezione2/riscaldamento/Es2.java
+++ Lezioni/src/lezione2/riscaldamento/Es2.java
@@ -1,6 +1,16 @@
 package lezione2.riscaldamento;
+
+import java.util.Scanner;
+
 /*Scrivere un programma che chiede in input un nome e stampa
 la prima lettera e l'ultima (suggerimento: substring)
  */
 public class Es2 {
+    public static void main(String[] args) {
+        Scanner tastiera = new Scanner(System.in);
+        System.out.println("Inserisci un nome ");
+        String nome = tastiera.nextLine();
+        System.out.println(nome.substring(0,1));
+        System.out.println(nome.substring(nome.length()-1));
+    }
 }
