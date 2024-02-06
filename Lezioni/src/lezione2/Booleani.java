25ad60f53333193435fb93172398658233b37ff9
fily1212 <fily1212@gmail.com>
2024-02-06 15:17:01 +0100

    condizioni

--- Lezioni/src/lezione2/Booleani.java
+++ Lezioni/src/lezione2/Booleani.java
@@ -31,16 +31,27 @@ public class Booleani {
         // stampare true se un voto insufficiente (cioè
         // tra 0 e 5
 
+        int numero = Integer.parseInt(tastiera.nextLine());
+        condizione = 0 <= numero && numero <= 5 ;
+        System.out.println(condizione);
+
         // chiedere un numero, true se è pari
+
+        numero = Integer.parseInt(tastiera.nextLine());
+        condizione = numero % 2 == 0 ;
+        System.out.println(condizione);
 
         // dato un anno, stampare true se è bisestile
         // un anno è bisestile se
-        // - è divisibile per 4 ma non per 100
-        // - oppure è divisibile per 400
+        // - è divisibile per 4 && ! per 100
+        // - || è divisibile per 400
         // 2024: SI
         // 1900: NO
         // 2023: NO
         // 2000: SI
+        numero = Integer.parseInt(tastiera.nextLine());
+        condizione = (numero % 4 == 0) && !(numero % 100 == 0) || (numero % 400 == 0)  ;
+        System.out.println(condizione);
 
     }
 }
